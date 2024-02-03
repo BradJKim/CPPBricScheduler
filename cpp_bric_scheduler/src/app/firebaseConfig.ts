43f94716fd1b7f546ccfdf20d4eff9564bd8908b
BradJKim <bradjkim03@gmail.com>
2024-02-02 16:12:00 -0800

    change variables to .env file

--- cpp_bric_scheduler/src/app/firebaseConfig.ts
+++ cpp_bric_scheduler/src/app/firebaseConfig.ts
@@ -4,19 +4,20 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth'
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBJDPjwNip7dR4QZLKw6cyMrBZ99XdfdrE",
-  authDomain: "cppbricscheduler.firebaseapp.com",
-  projectId: "cppbricscheduler",
-  storageBucket: "cppbricscheduler.appspot.com",
-  messagingSenderId: "643950148261",
-  appId: "1:643950148261:web:7f3d0fcfb972f19d46eb4c",
-  measurementId: "G-GZJP94E4SL"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
